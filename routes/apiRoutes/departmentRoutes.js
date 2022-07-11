ce36cc1f017b38c9eb21dbc47cb15ebdf5aece7f
Jack Atkerson <jatkerson18@gmail.com>
2022-07-11 17:27:23 -0500

    Updated department routes

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -17,6 +17,22 @@ router.get('/departments', (req, res) => {
     });
   });
 
+// GET a single department
+router.get('/department/:id', (req, res) => {
+    const sql = `SELECT * FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
 // DELETE a department
 router.delete('department/:id', (req ,res) => {
     const sql = `DELETE FROM departments WHERE id = ?`;
